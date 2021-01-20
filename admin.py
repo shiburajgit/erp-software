d3bc2d6434ee4aeb3e73528323537fd2fab2d418
shiburaj A <77714999+shiburajgit@users.noreply.github.com>
2021-01-20 12:02:01 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,28 +1,3 @@
 from django.contrib import admin
-from .models import Teacher, Student, Batch,Academicyear,Course,Fee,Category,Subcategory,Transport,Assign,Busdetails,Busstop
-from.models import Busroute
-from .models import Teacher, Student, Batch,Academicyear,Course,Timetable,Order
-from . import  models
 
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Batch)
-admin.site.register(Academicyear)
-admin.site.register(Course)
-admin.site.register(Timetable)
-admin.site.register(models.AttendanceMarked)
-admin.site.register(Order)
-admin.site.register(models.Attendance)
-admin.site.register(Fee)
-admin.site.register(Category)
-admin.site.register(Subcategory)
-admin.site.register(Transport)
-admin.site.register(Assign)
-admin.site.register(Busdetails)
-admin.site.register(Busstop)
-admin.site.register(Busroute)
-admin.site.register(models.Placement)
-admin.site.register(models.Quata)
-admin.site.register(models.PlacementBulk)
-admin.site.register(models.HigherStudies)
